f7038ea1854518c49fbb23bd3a352a5ce2119a85
kyechan99 <kyechan99@naver.com>
2017-07-21 01:19:16 +0900

    first init

---
+++ Script/User/UserData.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Net;
+using System.Net.Sockets;
+
+public enum MOVE_CONTROL
+{
+    STOP,
+    UP,
+    DOWN,
+    LEFT,
+    RIGHT,
+}
+
+namespace Server
+{
+    class UserData
+    {
+        public const int BUFFER_SIZE = 32768;    // 2바이트 대비
+
+        public Socket workSocket = null;                // 소켓
+        public byte[] buf = new byte[BUFFER_SIZE];      // 버퍼
+        public int recvLen = 0;                         // 데이터 길이
+    }
+}
